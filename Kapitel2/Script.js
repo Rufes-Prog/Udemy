6bd4bde8802431743d343eb463d03267b36771bd
Raphael Jansen <Jansen1991Raphael@gmail.com>
2025-02-19 10:28:58 +0100

    Add files via upload

---
+++ Kapitel2/Script.js
@@ -0,0 +1,14 @@
+"use strict";
+function add(arg1, arg2) {
+    return arg1 + arg2;
+}
+function concatenate(arg1, arg2) {
+    return arg1 + arg2;
+}
+const a1 = 2;
+const a2 = 'Raphael';
+const r1 = add(a1, a2);
+console.log(r1);
+const a3 = 'Jansen';
+const r2 = concatenate(a2, a3);
+console.log(r2);
---
+++ Kapitel2/Script.ts
@@ -0,0 +1,17 @@
+function add (arg1: number, arg2: number){
+    return arg1 + arg2;
+}
+
+function concatenate (arg1: string, arg2: string){
+    return arg1 + arg2;
+}
+
+const a1 = 2;
+const a2 = 'Raphael';
+
+const r1 = add(a1, a2);
+console.log(r1);
+
+const a3 = 'Jansen';
+const r2 = concatenate(a2, a3);
+console.log(r2);
