f701cf338b0c1137ddb6a1a95e2f49533f732613
Raphael Jansen <Jansen1991Raphael@gmail.com>
2025-02-19 10:50:44 +0100

    Add files via upload

---
+++ Kapitel1/ArrayTuple/Script.js
@@ -0,0 +1,12 @@
+"use strict";
+let arr = [1, 2, 3, 4, 5];
+console.log(arr);
+function printArray(arr) {
+    console.log(arr);
+}
+printArray(arr);
+function printTuple(Tpl) {
+    console.log(Tpl);
+}
+let Tpl = [27, 34, 'Raphael'];
+printTuple(Tpl);
